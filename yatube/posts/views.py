b8dfc9cf1ab4228966516d8d2cbb99b4b6dd44f6
lonees <ivan.baklag@gmail.com>
2023-02-27 13:59:33 +0800

    шаблоны
    Создали структуру шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, 'posts/index.html', context)
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
-    return HttpResponse(f'Группa {slug}')
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, 'posts/group_list.html', context)
