897dce30a53e84834cc0ae26a27e561a6b10a445
lonees <ivan.baklag@gmail.com>
2023-02-27 16:34:12 +0800

    update 27.02.2022
    Создана бд
    Настроена админ зона

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class AdminPost(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, AdminPost)
+
