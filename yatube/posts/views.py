6bc83c76ea64e5fa4370e1183e72de0bb7e9aafe
lonees <ivan.baklag@gmail.com>
2023-02-28 12:59:04 +0800

    completed version 1.0
    1.0

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-# Главная страница
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'posts': posts
+        'posts': posts,
+        'title': 'Последние обновления на сайте'
     }
     return render(request, 'posts/index.html', context)
 
 
-# Страница со списком мороженого
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': 'Здесь будет информация о группах проекта Yatube'
+        'group': group,
+        'posts': posts,
+        'title': group.title
     }
     return render(request, 'posts/group_list.html', context)
